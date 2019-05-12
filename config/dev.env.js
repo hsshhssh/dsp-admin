5e433c4bd2d5b85a383c94ae1c3c5b07f410bbed
samson.huang <samson.huang@yingzt.com>
2019-05-12 21:16:43 +0800

    更换登陆接口api

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://localhost:8080"',
+  LOGIN_BASE_API: '"http://localhost:8081"'
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
 })
