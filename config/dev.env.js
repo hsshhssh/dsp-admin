767960cd6ebe9b2ed08130660c73daf7d6995b59
samson.huang <samson.huang@yingzt.com>
2019-05-20 23:26:47 +0800

    fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"',
-  LOGIN_BASE_API: '"http://localhost:8081"'
+  BASE_API: '"http://localhost:8001"',
+  LOGIN_BASE_API: '"http://localhost:8002"'
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
 })
